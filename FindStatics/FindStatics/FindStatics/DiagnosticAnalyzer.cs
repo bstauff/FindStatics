a6eb2232d591db32be24166d82ea988a2981f4d3
Brian Stauff <brian.stauff.github@gmail.com>
2016-10-31 15:03:46 -0500

    Changed diagnostic severity to Warning

--- FindStatics/FindStatics/FindStatics/DiagnosticAnalyzer.cs
+++ FindStatics/FindStatics/FindStatics/DiagnosticAnalyzer.cs
@@ -22,7 +22,7 @@ namespace FindStatics
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Design";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
