941c2bfa466633a6ef2eae5b5b328c7c4dbcbcdf
Brian Stauff <brian.stauff.github@gmail.com>
2016-11-03 12:43:39 -0500

    Adding analyzer to find lock statements and remove them.  Not complete yet.

---
+++ FindStatics/FindStatics/FindStatics/FindLocksAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace FindStatics
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class FindLocksAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "RB003";
+
+        private static readonly string Title = "Lock statement found";
+        private static readonly string MessageFormat = "Lock statement should be avoided in Orleans environment";
+        private static readonly string Description = "Avoid using lock statements when working with Orleans.  Consider removing the lock.";
+        private const string Category = "Design";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, 
+            MessageFormat, Category, DiagnosticSeverity.Warning, true, Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeLockStatement, SyntaxKind.LockStatement);
+        }
+
+        private void AnalyzeLockStatement(SyntaxNodeAnalysisContext context)
+        {
+            var lockStatementNode = (LockStatementSyntax) context.Node;
+
+            var diagnostic = Diagnostic.Create(Rule, lockStatementNode.GetLocation());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
---
+++ FindStatics/FindStatics/FindStatics/FindLocksCodeFixProvider.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using System.Collections.Immutable;
+using System.Threading;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FindStatics
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(FindLocksCodeFixProvider)), Shared]
+    public class FindLocksCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Remove lock statement";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(FindLocksAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var lockStatementExpression =
+                root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<LockStatementSyntax>().First();
+
+            context.RegisterCodeFix(CodeAction.Create(title, c => RemoveLockAsync(context.Document, lockStatementExpression, c), title), diagnostic);
+        }
+
+        private async Task<Document> RemoveLockAsync(Document document, LockStatementSyntax lockStatement,
+            CancellationToken cancellationToken)
+        {
+            /*
+             * Save the block statement node from within the lock
+             * Remove the entire lock statement syntax node
+             * re insert the block statement node
+             */
+
+            var lockNodeBlockStatements = from x in lockStatement.ChildNodes()
+                                          where x is BlockSyntax
+                                          select x;
+
+            var lockNodeExpressionStatements = from BlockSyntax x in lockNodeBlockStatements
+                                            select x.Statements;
+
+            var allStatements = from SyntaxList<StatementSyntax> x in lockNodeExpressionStatements
+                                from StatementSyntax y in x
+                                select y;
+
+            var lockNodeParent = lockStatement.Parent;
+
+            var currentRoot = await document.GetSyntaxRootAsync();
+
+            var rootWithoutLockStatement = currentRoot.RemoveNode(lockStatement, SyntaxRemoveOptions.KeepNoTrivia);
+
+            //rootWithoutLockStatement.InsertNodesAfter(lockNodeParent, lockNodeExpressionStatements);
+
+            return null;
+        }
+    }
+}
