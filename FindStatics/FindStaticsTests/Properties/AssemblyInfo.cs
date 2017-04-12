c3f9ac6d7d99fa0f3f24aff2b0cc4e1f157f6c74
Brian Stauff <brian.stauff.github@gmail.com>
2017-04-12 11:55:43 -0500

    Added unit test project

---
+++ FindStatics/FindStaticsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FindStaticsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FindStaticsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4c43378-ffcf-46f7-a6d6-41c238b31b5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FindStatics/FindStaticsTests/StaticFieldsAnalyzerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindStatics;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace FindStaticsTests
+{
+    public class StaticFieldsAnalyzerTests : CodeFixVerifier
+    {
+        [Fact]
+        public void EnsureNoDiagnosticsShowWhenTheyShouldntTest()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new FindStaticFieldsCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new FindStaticFieldsAnalyzer();
+        }
+    }
+}
