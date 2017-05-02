c1d2bfc0cb7c71048a27129fe57b0b79404d8a55
Alex McCool <alex.mccool@yahoo.com>
2017-05-01 17:42:42 -0700

    IgnoreGeneratedCodeTest - should be an error, right?

--- FindStatics/FindStaticsTests/StaticFieldsAnalyzerTests.cs
+++ FindStatics/FindStaticsTests/StaticFieldsAnalyzerTests.cs
@@ -8,6 +8,7 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using TestHelper;
 using Xunit;
+using Microsoft.CodeAnalysis;
 
 namespace FindStaticsTests
 {
@@ -20,6 +21,99 @@ namespace FindStaticsTests
 
             VerifyCSharpDiagnostic(test);
         }
+
+
+
+        [Fact]
+        public void IgnoreGeneratedCodeTest()
+        {
+            var test = @"
+namespace crap
+    {
+
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute(""yy"", ""1.0.0.0"")]
+        class ItWasMeCodeGenThing
+        {
+            public static global::System.Object DeepCopier(global::System.Object original, global::System.Object context)
+            {
+                return new Object();
+            }
+            public static string Thing
+            {
+                get; set;
+            }
+        }
+    }
+
+";
+
+            //we want no error
+            VerifyCSharpDiagnostic(test);
+        }
+
+
+
+        [Fact]
+        public void FoundAStaticTest()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgram
+{
+    public class Class1
+    {
+        private static string Findme = ""ugh"";
+
+        public Class1()
+            {
+                Findme = ""sdgsgsgsg"";
+            }
+
+        public static string Thing
+        {
+            get; set;
+        }
+    }
+}
+";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "RB001",
+                Message = "Field 'Findme' is marked static",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 9)
+                    }
+            };
+
+            //we want no error
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
